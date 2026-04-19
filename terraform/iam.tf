9c6992e5c3333f5078cce6fb2de2f93fb0a41247
Liam <liam@hillefors.se>
2026-04-19 23:08:54 +0200

    Specify environment on IAM policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -89,7 +89,10 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main"]
+      values   = [
+        "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main",
+        "repo:${var.github_org}/${var.github_repo}:environment:production",
+      ]
     }
   }
 }
